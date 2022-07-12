7a6003a08c985c8953864d1171891666aaf9decf
lashadodashvili <107387918+lashadodashvili@users.noreply.github.com>
2022-07-12 16:11:09 +0400

    finished header

---
+++ main.js
@@ -0,0 +1,7 @@
+let searchButton = document.querySelector(".search-btn")
+let searchInput = document.querySelector(".search-input")
+
+searchButton.addEventListener("click", function () {
+    searchInput.classList.toggle("block")
+})
+
